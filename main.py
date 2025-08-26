1b2d1cd84949960e9d41c4544d280b0adb8a3f13
Ajay-Iyer10 <ajay.iyer@godigitaltc.com>
2025-08-26 10:35:37 +0530

    postgres conn

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from sqlalchemy import create_engine, text
 from datetime import datetime
 
 
-# DB_USER = 'DB_USER'
-# DB_PASSWORD = 'DB_PASSWORD'
-# DB = 'DB_1'
-# engine = create_engine(
-#         f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@172.23.0.2:5432/{DB}')
+DB_USER = 'concourse_user'
+DB_PASSWORD = 'concourse_pass'
+DB = 'concourse'
+engine = create_engine(
+        f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@172.23.0.2:5432/{DB}')
 code = 'RELIANCE.NS'
 comp_data = yf.Ticker(code)
 df = pd.DataFrame(comp_data.history(start = '2020-08-01', end = '2025-08-01'))
 df['stock_code'] = code
 df['company'] = 'Reliance Industries'
 df.reset_index(inplace=True)
+print('Data fetched successfully from yfinance')
+connection = engine.connect()
 print(df)
-# connection = engine.connect()
-# print(df)
-# df.to_sql(f'{table_name}', engine, if_exists='append',index=False)
-# connection.execute(text(f'ALTER TABLE {table_name} ADD FOREIGN KEY (company_id) REFERENCES comp_data(company_id);'))
-# connection.commit()
-# connection.close()
-# print(f'Table:{table_name} pushed to postgres')
+df['load_dttm'] = datetime.now()
+df.to_sql(f'con_raw_data', engine, if_exists='replace',index=False)
+connection.commit()
+connection.close()
+print(f'Table:con_raw_data pushed to postgres')
